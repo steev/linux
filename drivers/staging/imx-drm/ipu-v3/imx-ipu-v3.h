ac46a05c2f791ee7b557ff6a563245b653b8d845
Sascha Hauer <s.hauer@pengutronix.de>
2011-06-13 14:52:07 +0200

    staging: drm/imx: Add IC support to IPUv3
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/staging/imx-drm/ipu-v3/imx-ipu-v3.h
+++ drivers/staging/imx-drm/ipu-v3/imx-ipu-v3.h
@@ -332,4 +332,10 @@ struct ipu_client_platformdata {
 	int dma[2];
 };
 
+int ipu_image_convert(struct ipu_soc *ipu, struct ipu_image *in, struct ipu_image *out,
+		void (*complete)(void *ctx, int err), void *ctx);
+int ipu_image_deinterlace_convert(struct ipu_soc *ipu, struct ipu_image *in_p,
+		struct ipu_image *in, struct ipu_image *in_n, struct ipu_image *out,
+		void (*complete)(void *ctx, int err), void *complete_context);
+
 #endif /* __DRM_IPU_H__ */
