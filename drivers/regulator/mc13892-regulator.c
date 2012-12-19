165eff8e1ba8b75a7e0eb2c85aff5d13b2dc3ce4
Matt Sealey <matt@genesi-usa.com>
2012-12-19 17:23:56 -0600

    regulator: switchers vsel_shift is always 0 for this chip so shifting by it is redundant (step 2)

--- drivers/regulator/mc13892-regulator.c
+++ drivers/regulator/mc13892-regulator.c
@@ -440,8 +440,8 @@ static int mc13892_sw_regulator_get_voltage_sel(struct regulator_dev *rdev)
 
 	}
 
-	dev_dbg(rdev_get_dev(rdev), "%s post-lookup id: %d (used mask: 0x%08x shift: 0x%08x) val: %d\n", __func__, id,
-			mc13892_regulators[id].vsel_mask, mc13892_regulators[id].vsel_shift, val);
+	dev_dbg(rdev_get_dev(rdev), "%s post-lookup id: %d (used mask: 0x%08x) val: %d\n", __func__, id,
+			mc13892_regulators[id].vsel_mask, val);
 
 	return val;
 }
@@ -461,7 +461,7 @@ static int mc13892_sw_regulator_set_voltage_sel(struct regulator_dev *rdev,
 
 	volt = rdev->desc->volt_table[selector];
 	mask = mc13892_regulators[id].vsel_mask;
-	reg_value = selector << mc13892_regulators[id].vsel_shift; // note we do this again later for certain voltages, OPTIMIZE
+	reg_value = selector; // note we do this again later for certain voltages, OPTIMIZE
 
 	dev_dbg(rdev_get_dev(rdev), "%s pre-voltage-check volt: %d mask: 0x%08x value: 0x%08x\n", __func__, volt, mask, reg_value);
 
@@ -476,10 +476,10 @@ static int mc13892_sw_regulator_set_voltage_sel(struct regulator_dev *rdev,
 	if (mc13892_regulators[id].vsel_reg != MC13892_SWITCHERS0) {
 		if (volt > 1375000) {
 			selector -= MC13892_SWxHI_SEL_OFFSET;
-			reg_value = (selector << mc13892_regulators[id].vsel_shift) | MC13892_SWITCHERS0_SWxHI;
+			reg_value = selector | MC13892_SWITCHERS0_SWxHI;
 			mask |= MC13892_SWITCHERS0_SWxHI;
 		} else if (volt < 1100000) {
-			reg_value = (selector << mc13892_regulators[id].vsel_shift) & ~MC13892_SWITCHERS0_SWxHI;
+			reg_value = selector & ~MC13892_SWITCHERS0_SWxHI;
 			mask |= MC13892_SWITCHERS0_SWxHI;
 		}
 	}
