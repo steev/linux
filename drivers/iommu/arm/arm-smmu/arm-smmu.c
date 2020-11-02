randomplum/kernel-msm

WIP: iommu: arm-smmu: Set SCTLR.HUPCF bit
We seem to need to set either this or CFCFG (stall), otherwise gpu
faults trigger problems with other in-flight transactions from the
GPU causing CP errors, etc.

drivers/iommu/arm-smmu.c
@@ -724,6 +724,7 @@ static void arm_smmu_init_context_bank(struct arm_smmu_domain *smmu_domain,
 		reg |= SCTLR_CFCFG;    /* stall on fault */
 	if (stage1)
 		reg |= SCTLR_S1_ASIDPNE;
+	reg |= SCTLR_HUPCF;
 #ifdef __BIG_ENDIAN
 	reg |= SCTLR_E;
 #endif

steev/linux

drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -614,6 +614,7 @@ void arm_smmu_write_context_bank(struct arm_smmu_device *smmu, int idx)
 	      ARM_SMMU_SCTLR_TRE | ARM_SMMU_SCTLR_M;
 	if (stage1)
 		reg |= ARM_SMMU_SCTLR_S1_ASIDPNE;
+	reg |= ARM_SMMU_SCTLR_HUPCF;
 	if (IS_ENABLED(CONFIG_CPU_BIG_ENDIAN))
 		reg |= ARM_SMMU_SCTLR_E;
 