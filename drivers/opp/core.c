49f1f504dce7bcb6a5d8916ecc6dedf6f2a23cec
Dmitry Osipenko <digetx@gmail.com>
2021-08-15 19:43:44 +0300

    opp: Drop empty-table checks from _put functions
    [ Upstream commit c3ddfe66d2bb511f7fbcdc8e64952c7859e7e69d ]
    
    The current_opp is released only when whole OPP table is released,
    otherwise it's only marked as removed by dev_pm_opp_remove_table().
    Functions like dev_pm_opp_put_clkname() and dev_pm_opp_put_supported_hw()
    are checking whether OPP table is empty and it's not if current_opp is
    set since it holds the refcount of OPP, this produces a noisy warning
    from these functions about busy OPP table. Remove the checks to fix it.
    
    Cc: stable@vger.kernel.org
    Fixes: 81c4d8a3c414 ("opp: Keep track of currently programmed OPP")
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/opp/core.c
+++ drivers/opp/core.c
@@ -1846,9 +1846,6 @@ void dev_pm_opp_put_supported_hw(struct opp_table *opp_table)
 	if (unlikely(!opp_table))
 		return;
 
-	/* Make sure there are no concurrent readers while updating opp_table */
-	WARN_ON(!list_empty(&opp_table->opp_list));
-
 	kfree(opp_table->supported_hw);
 	opp_table->supported_hw = NULL;
 	opp_table->supported_hw_count = 0;
@@ -1904,9 +1901,6 @@ void dev_pm_opp_put_prop_name(struct opp_table *opp_table)
 {
 	if (unlikely(!opp_table))
 		return;
-
-	/* Make sure there are no concurrent readers while updating opp_table */
-	WARN_ON(!list_empty(&opp_table->opp_list));
 
 	kfree(opp_table->prop_name);
 	opp_table->prop_name = NULL;
@@ -2017,9 +2011,6 @@ void dev_pm_opp_put_regulators(struct opp_table *opp_table)
 	if (!opp_table->regulators)
 		goto put_opp_table;
 
-	/* Make sure there are no concurrent readers while updating opp_table */
-	WARN_ON(!list_empty(&opp_table->opp_list));
-
 	if (opp_table->enabled) {
 		for (i = opp_table->regulator_count - 1; i >= 0; i--)
 			regulator_disable(opp_table->regulators[i]);
@@ -2109,9 +2100,6 @@ void dev_pm_opp_put_clkname(struct opp_table *opp_table)
 	if (unlikely(!opp_table))
 		return;
 
-	/* Make sure there are no concurrent readers while updating opp_table */
-	WARN_ON(!list_empty(&opp_table->opp_list));
-
 	clk_put(opp_table->clk);
 	opp_table->clk = ERR_PTR(-EINVAL);
 
@@ -2182,9 +2170,6 @@ void dev_pm_opp_unregister_set_opp_helper(struct opp_table *opp_table)
 {
 	if (unlikely(!opp_table))
 		return;
-
-	/* Make sure there are no concurrent readers while updating opp_table */
-	WARN_ON(!list_empty(&opp_table->opp_list));
 
 	opp_table->set_opp = NULL;
 
