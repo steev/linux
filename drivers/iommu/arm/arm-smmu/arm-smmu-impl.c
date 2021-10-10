ed3d3e7b25587815f1a17a1398a326483bb0a614
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2021-10-10 05:33:50 +0300

    iommu: fix ARM_SMMU vs QCOM_SCM compilation
    After commit 424953cf3c66 ("qcom_scm: hide Kconfig symbol") arm-smmu got
    qcom_smmu_impl_init() call guarded by IS_ENABLED(CONFIG_ARM_SMMU_QCOM).
    However the CONFIG_ARM_SMMU_QCOM Kconfig entry does not exist, so the
    qcom_smmu_impl_init() is never called.
    
    So, let's fix this by always calling qcom_smmu_impl_init(). It does not
    touch the smmu passed unless the device is a non-Qualcomm one. Make
    ARM_SMMU select QCOM_SCM for ARCH_QCOM.
    
    Fixes: 424953cf3c66 ("qcom_scm: hide Kconfig symbol")
    Cc: Arnd Bergmann <arnd@arndb.de>
    Reported-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Steev Klimaszewski <steev@kali.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu-impl.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-impl.c
@@ -215,8 +215,13 @@ struct arm_smmu_device *arm_smmu_impl_init(struct arm_smmu_device *smmu)
 	    of_device_is_compatible(np, "nvidia,tegra186-smmu"))
 		return nvidia_smmu_impl_init(smmu);
 
-	if (IS_ENABLED(CONFIG_ARM_SMMU_QCOM))
-		smmu = qcom_smmu_impl_init(smmu);
+	/*
+	 * qcom_smmu_impl_init() will not touch smmu if the device is not
+	 * a Qualcomm one.
+	 */
+	smmu = qcom_smmu_impl_init(smmu);
+	if (IS_ERR(smmu))
+		return smmu;
 
 	if (of_device_is_compatible(np, "marvell,ap806-smmu-500"))
 		smmu->impl = &mrvl_mmu500_impl;
