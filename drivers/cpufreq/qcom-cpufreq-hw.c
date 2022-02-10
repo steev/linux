aebc61db47011c621f6e59548e0909929163ab37
Yury Norov <yury.norov@gmail.com>
2022-02-10 14:49:01 -0800

    cpufreq: replace cpumask_weight with cpumask_empty where appropriate
    drivers/cpufreq calls cpumask_weight() to check if any bit of a given
    cpumask is set. We can do it more efficiently with cpumask_empty() because
    cpumask_empty() stops traversing the cpumask as soon as it finds first set
    bit, while cpumask_weight() counts all bits unconditionally.
    
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com> (for SCMI cpufreq driver)

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -502,7 +502,7 @@ static int qcom_cpufreq_hw_cpu_init(struct cpufreq_policy *policy)
 	}
 
 	qcom_get_related_cpus(index, policy->cpus);
-	if (!cpumask_weight(policy->cpus)) {
+	if (cpumask_empty(policy->cpus)) {
 		dev_err(dev, "Domain-%d failed to get related CPUs\n", index);
 		ret = -ENOENT;
 		goto error;
