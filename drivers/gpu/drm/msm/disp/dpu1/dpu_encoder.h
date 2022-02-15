84d5db88ad04bb9909c264c7cd8501d2c56bb6c0
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2022-02-15 17:16:43 +0300

    drm/msm/dpu: simplify intf allocation code
    Rather than passing DRM_MODE_ENCODER_* and letting dpu_encoder to guess,
    which intf type we mean, pass INTF_DSI/INTF_DP directly.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Bjorn Andersson <bjorn.andersson@linaro.org>
    Signed-off-by: Steev Klimaszewski <steev@kali.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_encoder.h
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_encoder.h
@@ -20,7 +20,7 @@
 
 /**
  * struct msm_display_info - defines display properties
- * @intf_type:          DRM_MODE_ENCODER_ type
+ * @intf_type:          INTF_ type
  * @capabilities:       Bitmask of display flags
  * @num_of_h_tiles:     Number of horizontal tiles in case of split interface
  * @h_tile_instance:    Controller instance used per tile. Number of elements is
@@ -29,7 +29,7 @@
  *				 used instead of panel TE in cmd mode panels
  */
 struct msm_display_info {
-	int intf_type;
+	enum dpu_intf_type intf_type;
 	uint32_t capabilities;
 	uint32_t num_of_h_tiles;
 	uint32_t h_tile_instance[MAX_H_TILES_PER_DISPLAY];
