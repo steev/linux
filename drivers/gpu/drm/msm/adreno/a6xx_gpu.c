29c6793e99acce9d3da4e44a53892274cb31f288
Bjorn Andersson <bjorn.andersson@linaro.org>
2022-04-20 16:36:39 -0700

    drm/msm/adreno: Embryo to a690 support
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/gpu/drm/msm/adreno/a6xx_gpu.c
+++ drivers/gpu/drm/msm/adreno/a6xx_gpu.c
@@ -588,6 +588,59 @@ const struct adreno_reglist a660_hwcg[] = {
 	{},
 };
 
+const struct adreno_reglist a690_hwcg[] = {
+	{REG_A6XX_RBBM_CLOCK_CNTL_SP0, 0x02222222},
+	{REG_A6XX_RBBM_CLOCK_CNTL2_SP0, 0x02222220},
+	{REG_A6XX_RBBM_CLOCK_DELAY_SP0, 0x00000080},
+	{REG_A6XX_RBBM_CLOCK_HYST_SP0, 0x0000F3CF},
+	{REG_A6XX_RBBM_CLOCK_CNTL_TP0, 0x22222222},
+	{REG_A6XX_RBBM_CLOCK_CNTL2_TP0, 0x22222222},
+	{REG_A6XX_RBBM_CLOCK_CNTL3_TP0, 0x22222222},
+	{REG_A6XX_RBBM_CLOCK_CNTL4_TP0, 0x00022222},
+	{REG_A6XX_RBBM_CLOCK_DELAY_TP0, 0x11111111},
+	{REG_A6XX_RBBM_CLOCK_DELAY2_TP0, 0x11111111},
+	{REG_A6XX_RBBM_CLOCK_DELAY3_TP0, 0x11111111},
+	{REG_A6XX_RBBM_CLOCK_DELAY4_TP0, 0x00011111},
+	{REG_A6XX_RBBM_CLOCK_HYST_TP0, 0x77777777},
+	{REG_A6XX_RBBM_CLOCK_HYST2_TP0, 0x77777777},
+	{REG_A6XX_RBBM_CLOCK_HYST3_TP0, 0x77777777},
+	{REG_A6XX_RBBM_CLOCK_HYST4_TP0, 0x00077777},
+	{REG_A6XX_RBBM_CLOCK_CNTL_RB0, 0x22222222},
+	{REG_A6XX_RBBM_CLOCK_CNTL2_RB0, 0x01002222},
+	{REG_A6XX_RBBM_CLOCK_CNTL_CCU0, 0x00002220},
+	{REG_A6XX_RBBM_CLOCK_HYST_RB_CCU0, 0x00040F00},
+	{REG_A6XX_RBBM_CLOCK_CNTL_RAC, 0x25222022},
+	{REG_A6XX_RBBM_CLOCK_CNTL2_RAC, 0x00005555},
+	{REG_A6XX_RBBM_CLOCK_DELAY_RAC, 0x00000011},
+	{REG_A6XX_RBBM_CLOCK_HYST_RAC, 0x00445044},
+	{REG_A6XX_RBBM_CLOCK_CNTL_TSE_RAS_RBBM, 0x04222222},
+	{REG_A6XX_RBBM_CLOCK_MODE_VFD, 0x00002222},
+	{REG_A6XX_RBBM_CLOCK_MODE_GPC, 0x00222222},
+	{REG_A6XX_RBBM_CLOCK_DELAY_HLSQ_2, 0x00000002},
+	{REG_A6XX_RBBM_CLOCK_MODE_HLSQ, 0x00002222},
+	{REG_A6XX_RBBM_CLOCK_DELAY_TSE_RAS_RBBM, 0x00004000},
+	{REG_A6XX_RBBM_CLOCK_DELAY_VFD, 0x00002222},
+	{REG_A6XX_RBBM_CLOCK_DELAY_GPC, 0x00000200},
+	{REG_A6XX_RBBM_CLOCK_DELAY_HLSQ, 0x00000000},
+	{REG_A6XX_RBBM_CLOCK_HYST_TSE_RAS_RBBM, 0x00000000},
+	{REG_A6XX_RBBM_CLOCK_HYST_VFD, 0x00000000},
+	{REG_A6XX_RBBM_CLOCK_HYST_GPC, 0x04104004},
+	{REG_A6XX_RBBM_CLOCK_HYST_HLSQ, 0x00000000},
+	{REG_A6XX_RBBM_CLOCK_CNTL_TEX_FCHE, 0x00000222},
+	{REG_A6XX_RBBM_CLOCK_DELAY_TEX_FCHE, 0x00000111},
+	{REG_A6XX_RBBM_CLOCK_HYST_TEX_FCHE, 0x00000000},
+	{REG_A6XX_RBBM_CLOCK_CNTL_UCHE, 0x22222222},
+	{REG_A6XX_RBBM_CLOCK_HYST_UCHE, 0x00000004},
+	{REG_A6XX_RBBM_CLOCK_DELAY_UCHE, 0x00000002},
+	{REG_A6XX_RBBM_CLOCK_CNTL, 0x8AA8AA82},
+	{REG_A6XX_RBBM_ISDB_CNT, 0x00000182},
+	{REG_A6XX_RBBM_RAC_THRESHOLD_CNT, 0x00000000},
+	{REG_A6XX_RBBM_SP_HYST_CNT, 0x00000000},
+	{REG_A6XX_RBBM_CLOCK_CNTL_GMU_GX, 0x00000222},
+	{REG_A6XX_RBBM_CLOCK_DELAY_GMU_GX, 0x00000111},
+	{REG_A6XX_RBBM_CLOCK_HYST_GMU_GX, 0x00000555},
+};
+
 static void a6xx_set_hwcg(struct msm_gpu *gpu, bool state)
 {
 	struct adreno_gpu *adreno_gpu = to_adreno_gpu(gpu);
@@ -616,6 +669,13 @@ static void a6xx_set_hwcg(struct msm_gpu *gpu, bool state)
 
 	for (i = 0; (reg = &adreno_gpu->info->hwcg[i], reg->offset); i++)
 		gpu_write(gpu, reg->offset, state ? reg->value : 0);
+
+	if (adreno_is_a690(adreno_gpu)) {
+		gmu_write(gmu, REG_A6XX_GPU_GMU_AO_GMU_CGC_MODE_CNTL, 0x00020200);
+		gmu_write(gmu, REG_A6XX_GPU_GMU_AO_GMU_CGC_DELAY_CNTL, 0x00010111);
+		gmu_write(gmu, REG_A6XX_GPU_GMU_AO_GMU_CGC_HYST_CNTL, 0x00005555);
+		gmu_write(gmu, 0x5002, 0x0);
+	}
 
 	/* Enable SP clock */
 	gmu_rmw(gmu, REG_A6XX_GPU_GMU_GX_SPTPRAC_CLOCK_CONTROL, 0, 1);
@@ -747,6 +807,59 @@ static const u32 a660_protect[] = {
 	A6XX_PROTECT_NORDWR(0x1f8c0, 0x0000), /* note: infinite range */
 };
 
+/* A690 protected register list */
+static const u32 a690_protect[] = {
+	A6XX_PROTECT_RDONLY(0x00000, 0x004ff),
+	A6XX_PROTECT_RDONLY(0x00501, 0x00506),
+	A6XX_PROTECT_RDONLY(0x0050b, 0x007ff),
+	A6XX_PROTECT_NORDWR(0x0050e, 0x0050e),
+	A6XX_PROTECT_NORDWR(0x00510, 0x00510),
+	A6XX_PROTECT_NORDWR(0x00534, 0x00534),
+	A6XX_PROTECT_NORDWR(0x00800, 0x00882),
+	A6XX_PROTECT_NORDWR(0x008a0, 0x008a8),
+	A6XX_PROTECT_NORDWR(0x008ab, 0x008cf),
+	A6XX_PROTECT_RDONLY(0x008d0, 0x0098c),
+	A6XX_PROTECT_NORDWR(0x00900, 0x0094d),
+	A6XX_PROTECT_NORDWR(0x0098d, 0x00bff),
+	A6XX_PROTECT_NORDWR(0x00e00, 0x00e01),
+	A6XX_PROTECT_NORDWR(0x00e03, 0x00e0f),
+	A6XX_PROTECT_NORDWR(0x03c00, 0x03cc3),
+	A6XX_PROTECT_RDONLY(0x03cc4, 0x05cc3),
+	A6XX_PROTECT_NORDWR(0x08630, 0x087ff),
+	A6XX_PROTECT_NORDWR(0x08e00, 0x08e00),
+	A6XX_PROTECT_NORDWR(0x08e08, 0x08e08),
+	A6XX_PROTECT_NORDWR(0x08e50, 0x08e6f),
+	A6XX_PROTECT_NORDWR(0x08e80, 0x090ff),
+	A6XX_PROTECT_NORDWR(0x09624, 0x097ff),
+	A6XX_PROTECT_NORDWR(0x09e60, 0x09e71),
+	A6XX_PROTECT_NORDWR(0x09e78, 0x09fff),
+	A6XX_PROTECT_NORDWR(0x0a630, 0x0a7ff),
+	A6XX_PROTECT_NORDWR(0x0ae02, 0x0ae02),
+	A6XX_PROTECT_NORDWR(0x0ae50, 0x0af7f),
+	A6XX_PROTECT_NORDWR(0x0b604, 0x0b604),
+	A6XX_PROTECT_NORDWR(0x0b608, 0x0b60e),
+	A6XX_PROTECT_NORDWR(0x0be02, 0x0be03),
+	A6XX_PROTECT_NORDWR(0x0be20, 0x0bf7f),
+	A6XX_PROTECT_NORDWR(0x0d000, 0x0d5ff),
+	A6XX_PROTECT_NORDWR(0x0f000, 0x0fbff),
+	A6XX_PROTECT_RDONLY(0x0fc00, 0x11bff),
+	/* Note1:  lastspanunbound feature is enabled in
+	 *         CP_PROTECT_CNTL and hence this last
+	 *         protect register(REG_47) has infinite
+	 *         span.
+	 *
+	 * Note2:  Although we are protecting the SMMU
+	 *         range here the CP register protection
+	 *         interrupt will not fire for this range
+	 *         as GPU RAP can only cover the GPU 18-bit
+	 *         DW address space.  So max address offset
+	 *         is 0x3FFFF.  Also note that the max number
+	 *         of bits for address in violation in
+	 *         CP_PROT_STATUS is only 18.
+	 */
+	A6XX_PROTECT_NORDWR(0x11c00, 0x00000),
+};
+
 static void a6xx_set_cp_protect(struct msm_gpu *gpu)
 {
 	struct adreno_gpu *adreno_gpu = to_adreno_gpu(gpu);
@@ -758,6 +871,11 @@ static void a6xx_set_cp_protect(struct msm_gpu *gpu)
 		count = ARRAY_SIZE(a650_protect);
 		count_max = 48;
 		BUILD_BUG_ON(ARRAY_SIZE(a650_protect) > 48);
+	} else if (adreno_is_a690(adreno_gpu)) {
+		regs = a690_protect;
+		count = ARRAY_SIZE(a690_protect);
+		count_max = 48;
+		BUILD_BUG_ON(ARRAY_SIZE(a690_protect) > 48);
 	} else if (adreno_is_a660_family(adreno_gpu)) {
 		regs = a660_protect;
 		count = ARRAY_SIZE(a660_protect);
@@ -1064,6 +1182,13 @@ static int hw_init(struct msm_gpu *gpu)
 
 	if (adreno_is_a660_family(adreno_gpu))
 		gpu_write(gpu, REG_A6XX_CP_LPAC_PROG_FIFO_SIZE, 0x00000020);
+
+#define REG_A6XX_RBBM_LPAC_GBIF_CLIENT_QOS_CNTL      0x5ff
+	if (adreno_is_a690(adreno_gpu)) {
+		gpu_write(gpu, REG_A6XX_RBBM_GBIF_CLIENT_QOS_CNTL, 0x0);
+		gpu_write(gpu, REG_A6XX_RBBM_LPAC_GBIF_CLIENT_QOS_CNTL, 0x0);
+		gpu_write(gpu, REG_A6XX_CP_LPAC_PROG_FIFO_SIZE, 0x00000020);
+	}
 
 	/* Setting the mem pool size */
 	gpu_write(gpu, REG_A6XX_CP_MEM_POOL_SIZE, 128);
