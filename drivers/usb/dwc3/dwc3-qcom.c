937c39e62dbc3b264a2ad21c71d4615c28635cea
Johan Hovold <johan+linaro@kernel.org>
2022-08-02 10:19:19 +0200

    usb: dwc3: qcom: clean up suspend callbacks
    Clean up the suspend callbacks by separating the error and success paths
    to improve readability.
    
    Also drop a related redundant initialisation.
    
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>

--- drivers/usb/dwc3/dwc3-qcom.c
+++ drivers/usb/dwc3/dwc3-qcom.c
@@ -959,12 +959,11 @@ static int __maybe_unused dwc3_qcom_pm_suspend(struct device *dev)
 {
 	struct dwc3_qcom *qcom = dev_get_drvdata(dev);
 	bool wakeup = device_may_wakeup(dev);
-	int ret = 0;
-
+	int ret;
 
 	ret = dwc3_qcom_suspend(qcom, wakeup);
-	if (!ret)
-		qcom->pm_suspended = true;
+	if (ret)
+		return ret;
 
 	qcom->pm_suspended = true;
 
@@ -978,8 +977,8 @@ static int __maybe_unused dwc3_qcom_pm_resume(struct device *dev)
 	int ret;
 
 	ret = dwc3_qcom_resume(qcom, wakeup);
-	if (!ret)
-		qcom->pm_suspended = false;
+	if (ret)
+		return ret;
 
 	qcom->pm_suspended = false;
 
