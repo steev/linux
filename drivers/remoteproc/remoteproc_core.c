acfd99ca1fc0cdc47d57af0bbaf17c0ae5e22f75
Maria Yu <quic_aiquny@quicinc.com>
2022-09-09 16:33:33 +0800

    remoteproc: core: do pm relax when not first crash
    Even if it is not first crash, need to relax the pm
    wakelock otherwise the device will stay awake.
    
    Signed-off-by: Maria Yu <quic_aiquny@quicinc.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -1956,6 +1956,7 @@ static void rproc_crash_handler_work(struct work_struct *work)
 	if (rproc->state == RPROC_CRASHED || rproc->state == RPROC_OFFLINE) {
 		/* handle only the first crash detected */
 		mutex_unlock(&rproc->lock);
+		pm_relax(rproc->dev.parent);
 		return;
 	}
 
