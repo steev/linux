91af1d6c497b7d87433d151421929f68dd358ebb
Gwendal Grignou <gwendal@chromium.org>
2023-01-28 16:37:53 -0800

    iio: proximity: sx_common: Add old register mapping
    Older firmwares still send sensor configuration using a list of
    registers with opaque values defined during sensor tuning.
    sx9234 and sx9360 sensor on ACPI based devices are concerned.
    More schema to configure the sensors will be needed to support devices
    designed for windows, like Samsung Galaxy Book2.
    
    Support schema is: "<_HID>.<register_name>". For instance
    "STH9324,reg_adv_ctrl2" in:
    
        Scope (\_SB.PCI0.I2C2)
        {
            Device (SX28)
            {
                Name (_HID, "STH9324")  // _HID: Hardware ID
    ...
                Name (_DSD, Package (0x02)  // _DSD: Device-Specific Data
                {
                    ToUUID ("daffd814-6eba-4d8c-8a91-bc9bbf4aa301") /*
    Device Properties for _DSD */,
                    Package (0x3F)
                    {
    ...
                        Package (0x02)
                        {
                            "STH9324,reg_adv_ctrl2",
                            Zero
                        },`
    
    Signed-off-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Link: https://lore.kernel.org/r/20230129003753.1980309-1-gwendal@chromium.org
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/proximity/sx_common.c
+++ drivers/iio/proximity/sx_common.c
@@ -5,6 +5,7 @@
  * Common part of most Semtech SAR sensor.
  */
 
+#include <linux/acpi.h>
 #include <linux/bitops.h>
 #include <linux/byteorder/generic.h>
 #include <linux/delay.h>
@@ -423,6 +424,25 @@ static const struct iio_buffer_setup_ops sx_common_buffer_setup_ops = {
 	.preenable = sx_common_buffer_preenable,
 	.postdisable = sx_common_buffer_postdisable,
 };
+
+void sx_common_get_raw_register_config(struct device *dev,
+				       struct sx_common_reg_default *reg_def)
+{
+	struct acpi_device *adev = ACPI_COMPANION(dev);
+	u32 raw = 0, ret;
+	char prop[80];
+
+	if (!reg_def->property || !adev)
+		return;
+
+	snprintf(prop, ARRAY_SIZE(prop), "%s,reg_%s", acpi_device_hid(adev), reg_def->property);
+	ret = device_property_read_u32(dev, prop, &raw);
+	if (ret)
+		return;
+
+	reg_def->def = raw;
+}
+EXPORT_SYMBOL_NS_GPL(sx_common_get_raw_register_config, SEMTECH_PROX);
 
 #define SX_COMMON_SOFT_RESET				0xde
 
