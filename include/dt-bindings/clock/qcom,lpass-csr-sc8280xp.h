e50993c6580cc57e2361bee549dde32f7145d784
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2023-02-15 14:28:54 +0000

    clk: qcom: sc8280xp: add lpass csr driver
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

---
+++ include/dt-bindings/clock/qcom,lpass-csr-sc8280xp.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2022, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLK_QCOM_LPASS_AUDIO_CC_SC8280XP_H
+#define _DT_BINDINGS_CLK_QCOM_LPASS_AUDIO_CC_SC8280XP_H
+
+/* LPASS AUDIO CC CSR */
+#define LPASS_AUDIO_SWR_RX_CGCR				0
+#define LPASS_AUDIO_SWR_WSA_CGCR			1
+#define LPASS_AUDIO_SWR_WSA2_CGCR			2
+
+/* LPASS TCSR */
+#define LPASS_AUDIO_SWR_TX_CGCR				0
+
+#endif
