bf2e9ded6d7a07f93376d379e1076ca1a2bce7ae
Konrad Dybcio <konrad.dybcio@linaro.org>
2023-04-25 11:12:40 +0000

    dt-bindings: clock: Add SC8280XP VIDEOCC
    SC8280XP, like most recent higher-end chips has a separate clock
    controller block just for the Venus IP. Document it.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@linaro.org>
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>

---
+++ include/dt-bindings/reset/qcom,sc8280xp-videocc.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2021, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_RESET_QCOM_VIDEO_CC_SC8280XP_H
+#define _DT_BINDINGS_RESET_QCOM_VIDEO_CC_SC8280XP_H
+
+#define VIDEO_CC_CVP_INTERFACE_BCR			0
+#define VIDEO_CC_CVP_MVS0_BCR				1
+#define VIDEO_CC_CVP_MVS0C_BCR				2
+#define VIDEO_CC_CVP_MVS1_BCR				3
+#define VIDEO_CC_CVP_MVS1C_BCR				4
+#define VIDEO_CC_MVS0C_CLK_ARES				5
+#define VIDEO_CC_MVS1C_CLK_ARES				6
+
+#endif
