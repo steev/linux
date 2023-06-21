a95966d23b75635c17619090b559232778c5cd44
Krishna Kurapati <quic_kriskura@quicinc.com>
2023-06-21 10:06:23 +0530

    usb: dwc3: core: Refactor PHY logic to support Multiport Controller
    Currently the DWC3 driver supports only single port controller
    which requires at most one HS and one SS PHY.
    
    But the DWC3 USB controller can be connected to multiple ports and
    each port can have their own PHYs. Each port of the multiport
    controller can either be HS+SS capable or HS only capable
    Proper quantification of them is required to modify GUSB2PHYCFG
    and GUSB3PIPECTL registers appropriately.
    
    Add support for detecting, obtaining and configuring phy's supported
    by a multiport controller and limit the max number of ports
    supported to 4.
    
    Signed-off-by: Harsh Agarwal <quic_harshq@quicinc.com>
    [Krishna: Modifed logic for generic phy and rebased the patch]
    Signed-off-by: Krishna Kurapati <quic_kriskura@quicinc.com>
    Acked-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Link: https://lore.kernel.org/r/20230621043628.21485-6-quic_kriskura@quicinc.com
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -37,6 +37,9 @@
 #define XHCI_EXT_PORT_MINOR(x)	(((x) >> 16) & 0xff)
 #define XHCI_EXT_PORT_COUNT(x)	(((x) >> 8) & 0xff)
 
+/* Number of ports supported by a multiport controller */
+#define DWC3_MAX_PORTS 4
+
 #define DWC3_MSG_MAX	500
 
 /* Global constants */
@@ -1028,8 +1031,8 @@ struct dwc3_scratchpad_array {
  * @usb_psy: pointer to power supply interface.
  * @usb2_phy: pointer to USB2 PHY
  * @usb3_phy: pointer to USB3 PHY
- * @usb2_generic_phy: pointer to USB2 PHY
- * @usb3_generic_phy: pointer to USB3 PHY
+ * @usb2_generic_phy: pointer to array of USB2 PHY
+ * @usb3_generic_phy: pointer to array of USB3 PHY
  * @num_usb2_ports: number of USB2 ports.
  * @num_usb3_ports: number of USB3 ports.
  * @phys_ready: flag to indicate that PHYs are ready
@@ -1168,8 +1171,8 @@ struct dwc3 {
 	struct usb_phy		*usb2_phy;
 	struct usb_phy		*usb3_phy;
 
-	struct phy		*usb2_generic_phy;
-	struct phy		*usb3_generic_phy;
+	struct phy		*usb2_generic_phy[DWC3_MAX_PORTS];
+	struct phy		*usb3_generic_phy[DWC3_MAX_PORTS];
 
 	u8			num_usb2_ports;
 	u8			num_usb3_ports;
