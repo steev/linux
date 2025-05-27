f5cbf1e8597f906e5d79e8753c0cf3ffd35b50f8
Kornel Dulęba <korneld@google.com>
2025-05-27 12:18:07 +0000

    power: supply: qcom_battmgr: Report battery capacity
    Battery charge can be reported in several different ways. One of them is
    is charge percentage referred to as POWER_SUPPLY_PROP_CAPACITY in the
    power supply API. Currently the driver reports the capacity in this way
    on SM8350, but not on the newer variants referred to as SC8280XP in the
    driver. Although this is not a bug in itself, not reporting the
    percentage can confuse some userspace consumers.
    Mimic what is done in the ACPI driver (drivers/acpi/battery.c) and
    calculate the percentage capacity by dividing the current charge value
    by the full charge. Both values are expressed in either uWh, or
    in uAh.
    
    Signed-off-by: Kornel Dulęba <korneld@google.com>
    Link: https://lore.kernel.org/r/20250527121807.3597061-1-korneld@google.com

--- drivers/power/supply/qcom_battmgr.c
+++ drivers/power/supply/qcom_battmgr.c
@@ -577,6 +577,8 @@ static int qcom_battmgr_bat_get_property(struct power_supply *psy,
 		val->intval = battmgr->status.capacity;
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
+		if (battmgr->status.percent == (unsigned int)-1)
+			return -ENODATA;
 		val->intval = battmgr->status.percent;
 		break;
 	case POWER_SUPPLY_PROP_TEMP:
@@ -617,6 +619,7 @@ static const enum power_supply_property sc8280xp_bat_props[] = {
 	POWER_SUPPLY_PROP_STATUS,
 	POWER_SUPPLY_PROP_PRESENT,
 	POWER_SUPPLY_PROP_TECHNOLOGY,
+	POWER_SUPPLY_PROP_CAPACITY,
 	POWER_SUPPLY_PROP_CYCLE_COUNT,
 	POWER_SUPPLY_PROP_VOLTAGE_MAX_DESIGN,
 	POWER_SUPPLY_PROP_VOLTAGE_NOW,
@@ -1063,6 +1066,21 @@ static void qcom_battmgr_sc8280xp_callback(struct qcom_battmgr *battmgr,
 		battmgr->ac.online = source == BATTMGR_CHARGING_SOURCE_AC;
 		battmgr->usb.online = source == BATTMGR_CHARGING_SOURCE_USB;
 		battmgr->wireless.online = source == BATTMGR_CHARGING_SOURCE_WIRELESS;
+		if (battmgr->info.last_full_capacity != 0) {
+			/*
+			 * 100 * battmgr->status.capacity can overflow a 32bit
+			 * unsigned integer. Do a temporary cast to avoid that.
+			 */
+			battmgr->status.percent =
+				(uint64_t)100 * battmgr->status.capacity /
+				battmgr->info.last_full_capacity;
+		} else {
+			/*
+			 * Let the sysfs handler know no data is available at
+			 * this time.
+			 */
+			battmgr->status.percent = (unsigned int)-1;
+		}
 		break;
 	case BATTMGR_BAT_DISCHARGE_TIME:
 		battmgr->status.discharge_time = le32_to_cpu(resp->time);
