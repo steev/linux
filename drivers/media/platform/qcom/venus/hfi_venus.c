043b486c3d2c17d235561fcb5fb37cddc1fb3fd0
Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
2025-06-12 10:29:43 +0200

    media: venus: hfi: explicitly release IRQ during teardown
    Ensure the IRQ is released before dismantling the ISR handler and
    clearing related pointers.
    
    This prevents any possibility of the interrupt triggering after the
    handler context has been invalidated.
    
    Fixes: d96d3f30c0f2 ("[media] media: venus: hfi: add Venus HFI files")
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20250612082943.2753676-1-jorge.ramirez@oss.qualcomm.com

--- drivers/media/platform/qcom/venus/hfi_venus.c
+++ drivers/media/platform/qcom/venus/hfi_venus.c
@@ -1687,6 +1687,7 @@ void venus_hfi_destroy(struct venus_core *core)
 	venus_interface_queues_release(hdev);
 	mutex_destroy(&hdev->lock);
 	kfree(hdev);
+	devm_free_irq(core->dev, core->irq, core);
 	core->ops = NULL;
 }
 
