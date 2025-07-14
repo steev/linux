67bca023ca2e88c0180acceb467c4f91e8f3e612
Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
2025-07-14 23:31:04 +0530

    PCI: qcom: Switch to bus notifier for enabling ASPM of PCI devices
    Commit 9f4f3dfad8cf ("PCI: qcom: Enable ASPM for platforms supporting 1.9.0
    ops") allowed the Qcom controller driver to enable ASPM for all PCI devices
    enumerated at the time of the controller driver probe. It proved to be
    useful for devices already powered on by the bootloader as it allowed
    devices to enter ASPM without user intervention.
    
    However, it could not enable ASPM for the hotplug capable devices i.e.,
    devices enumerated *after* the controller driver probe. This limitation
    mostly went unnoticed as the Qcom PCI controllers are not hotplug capable
    and also the bootloader has been enabling the PCI devices before Linux
    Kernel boots (mostly on the Qcom compute platforms which users use on a
    daily basis).
    
    But with the advent of the commit b458ff7e8176 ("PCI/pwrctl: Ensure that
    pwrctl drivers are probed before PCI client drivers"), the pwrctrl driver
    started to block the PCI device enumeration until it had been probed.
    Though, the intention of the commit was to avoid race between the pwrctrl
    driver and PCI client driver, it also meant that the pwrctrl controlled PCI
    devices may get probed after the controller driver and will no longer have
    ASPM enabled. So users started noticing high runtime power consumption with
    WLAN chipsets on Qcom compute platforms like Thinkpad X13s, and Thinkpad
    T14s, etc...
    
    Obviously, it is the pwrctrl change that caused regression, but it
    ultimately uncovered a flaw in the ASPM enablement logic of the controller
    driver. So to address the actual issue, switch to the bus notifier for
    enabling ASPM of the PCI devices. The notifier will notify the controller
    driver when a PCI device is attached to the bus, thereby allowing it to
    enable ASPM more reliably. It should be noted that the
    'pci_dev::link_state', which is required for enabling ASPM by the
    pci_enable_link_state_locked() API, is only set by the time of
    BUS_NOTIFY_BIND_DRIVER stage of the notification. So we cannot enable ASPM
    during BUS_NOTIFY_ADD_DEVICE stage.
    
    So with this, we can also get rid of the controller driver specific
    'qcom_pcie_ops::host_post_init' callback.
    
    Cc: stable@vger.kernel.org # v6.7
    Fixes: 9f4f3dfad8cf ("PCI: qcom: Enable ASPM for platforms supporting 1.9.0 ops")
    Reported-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20250714-aspm_fix-v1-1-7d04b8c140c8@oss.qualcomm.com

--- drivers/pci/controller/dwc/pcie-qcom.c
+++ drivers/pci/controller/dwc/pcie-qcom.c
@@ -21,6 +21,7 @@
 #include <linux/limits.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/notifier.h>
 #include <linux/of.h>
 #include <linux/pci.h>
 #include <linux/pm_opp.h>
@@ -254,7 +255,6 @@ struct qcom_pcie_ops {
 	int (*get_resources)(struct qcom_pcie *pcie);
 	int (*init)(struct qcom_pcie *pcie);
 	int (*post_init)(struct qcom_pcie *pcie);
-	void (*host_post_init)(struct qcom_pcie *pcie);
 	void (*deinit)(struct qcom_pcie *pcie);
 	void (*ltssm_enable)(struct qcom_pcie *pcie);
 	int (*config_sid)(struct qcom_pcie *pcie);
@@ -284,6 +284,7 @@ struct qcom_pcie {
 	struct icc_path *icc_cpu;
 	const struct qcom_pcie_cfg *cfg;
 	struct dentry *debugfs;
+	struct notifier_block nb;
 	bool suspended;
 	bool use_pm_opp;
 	bool soc_is_rpmh;
@@ -1044,25 +1045,6 @@ static int qcom_pcie_post_init_2_7_0(struct qcom_pcie *pcie)
 	return 0;
 }
 
-static int qcom_pcie_enable_aspm(struct pci_dev *pdev, void *userdata)
-{
-	/*
-	 * Downstream devices need to be in D0 state before enabling PCI PM
-	 * substates.
-	 */
-	pci_set_power_state_locked(pdev, PCI_D0);
-	pci_enable_link_state_locked(pdev, PCIE_LINK_STATE_ALL);
-
-	return 0;
-}
-
-static void qcom_pcie_host_post_init_2_7_0(struct qcom_pcie *pcie)
-{
-	struct dw_pcie_rp *pp = &pcie->pci->pp;
-
-	pci_walk_bus(pp->bridge->bus, qcom_pcie_enable_aspm, NULL);
-}
-
 static void qcom_pcie_deinit_2_7_0(struct qcom_pcie *pcie)
 {
 	struct qcom_pcie_resources_2_7_0 *res = &pcie->res.v2_7_0;
@@ -1319,19 +1301,9 @@ static void qcom_pcie_host_deinit(struct dw_pcie_rp *pp)
 	pcie->cfg->ops->deinit(pcie);
 }
 
-static void qcom_pcie_host_post_init(struct dw_pcie_rp *pp)
-{
-	struct dw_pcie *pci = to_dw_pcie_from_pp(pp);
-	struct qcom_pcie *pcie = to_qcom_pcie(pci);
-
-	if (pcie->cfg->ops->host_post_init)
-		pcie->cfg->ops->host_post_init(pcie);
-}
-
 static const struct dw_pcie_host_ops qcom_pcie_dw_ops = {
 	.init		= qcom_pcie_host_init,
 	.deinit		= qcom_pcie_host_deinit,
-	.post_init	= qcom_pcie_host_post_init,
 	.pme_turn_off	= qcom_pcie_pme_turn_off,
 };
 
@@ -1394,7 +1366,6 @@ static const struct qcom_pcie_ops ops_1_9_0 = {
 	.get_resources = qcom_pcie_get_resources_2_7_0,
 	.init = qcom_pcie_init_2_7_0,
 	.post_init = qcom_pcie_post_init_2_7_0,
-	.host_post_init = qcom_pcie_host_post_init_2_7_0,
 	.deinit = qcom_pcie_deinit_2_7_0,
 	.ltssm_enable = qcom_pcie_2_3_2_ltssm_enable,
 	.config_sid = qcom_pcie_config_sid_1_9_0,
@@ -1405,7 +1376,6 @@ static const struct qcom_pcie_ops ops_1_21_0 = {
 	.get_resources = qcom_pcie_get_resources_2_7_0,
 	.init = qcom_pcie_init_2_7_0,
 	.post_init = qcom_pcie_post_init_2_7_0,
-	.host_post_init = qcom_pcie_host_post_init_2_7_0,
 	.deinit = qcom_pcie_deinit_2_7_0,
 	.ltssm_enable = qcom_pcie_2_3_2_ltssm_enable,
 };
@@ -1619,6 +1589,33 @@ static void qcom_pcie_deinit_debugfs(struct qcom_pcie *pcie)
 	debugfs_remove(pcie->debugfs);
 }
 
+static int qcom_pcie_enable_aspm(struct pci_dev *pdev)
+{
+	/*
+	 * Downstream devices need to be in D0 state before enabling PCI PM
+	 * substates.
+	 */
+	pci_set_power_state_locked(pdev, PCI_D0);
+	pci_enable_link_state_locked(pdev, PCIE_LINK_STATE_ALL);
+
+	return 0;
+}
+
+static int pcie_qcom_notify(struct notifier_block *nb, unsigned long action,
+			      void *data)
+{
+	struct qcom_pcie *pcie = container_of(nb, struct qcom_pcie, nb);
+	struct device *dev = data;
+	struct pci_dev *pdev = to_pci_dev(dev);
+
+	switch (action) {
+	case BUS_NOTIFY_BIND_DRIVER:
+		qcom_pcie_enable_aspm(pdev);
+		break;
+	}
+
+	return NOTIFY_DONE;
+}
 static int qcom_pcie_probe(struct platform_device *pdev)
 {
 	const struct qcom_pcie_cfg *pcie_cfg;
@@ -1746,10 +1743,15 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, pcie);
 
+	pcie->nb.notifier_call = pcie_qcom_notify;
+	ret = bus_register_notifier(&pci_bus_type, &pcie->nb);
+	if (ret)
+		goto err_phy_exit;
+
 	ret = dw_pcie_host_init(pp);
 	if (ret) {
 		dev_err(dev, "cannot initialize host\n");
-		goto err_phy_exit;
+		goto err_unregister_notifier;
 	}
 
 	name = devm_kasprintf(dev, GFP_KERNEL, "qcom_pcie_global_irq%d",
@@ -1785,6 +1787,8 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 
 err_host_deinit:
 	dw_pcie_host_deinit(pp);
+err_unregister_notifier:
+	bus_unregister_notifier(&pci_bus_type, &pcie->nb);
 err_phy_exit:
 	phy_exit(pcie->phy);
 err_pm_runtime_put:
