4c8d3f53ab892bc1fb85f0e286eabb577e9d8ae8
Stephan Gerhold <stephan.gerhold@linaro.org>
2025-07-25 15:21:40 +0200

    remoteproc: core: Allow restarting detached remoteprocs with new firmware
    At the moment, the remoteproc core supports only one auto boot "strategy":
    A remoteproc that is already running during boot ("detached") is attached,
    a remoteproc that is offline is started after loading the firmware. This
    works if the firmware loaded during boot is the same that we would start
    later, but it could also be outdated or a reduced size version that is
    missing some functionality. In this case, the best option is to try
    restarting it with new firmware - assuming that it is available while
    booting.
    
    Add support for this alternative behavior by replacing the "auto_boot" bool
    with a more explicit enum rproc_auto_boot.
    
    For RPROC_AUTO_BOOT_RESTART_IF_FW_AVAILABLE, try requesting the firmware
    early and - if successful - perform a clean stop of the remoteproc so that
    it can be restarted with the new firmware afterwards. A remoteproc driver
    making use of this functionality must handle the stop() callback being
    called during the initial detached state.
    
    Signed-off-by: Stephan Gerhold <stephan.gerhold@linaro.org>
    Signed-off-by: Abel Vesa <abel.vesa@oss.qualcomm.com>

--- drivers/remoteproc/qcom_q6v5_pas.c
+++ drivers/remoteproc/qcom_q6v5_pas.c
@@ -774,7 +774,10 @@ static int qcom_pas_probe(struct platform_device *pdev)
 	}
 
 	rproc->has_iommu = of_property_present(pdev->dev.of_node, "iommus");
-	rproc->auto_boot = desc->auto_boot;
+	if (desc->auto_boot)
+		rproc->auto_boot = RPROC_AUTO_BOOT_ATTACH_OR_START;
+	else
+		rproc->auto_boot = RPROC_AUTO_BOOT_DISABLED;
 	rproc_coredump_set_elf_info(rproc, ELFCLASS32, EM_NONE);
 
 	pas = rproc->priv;
