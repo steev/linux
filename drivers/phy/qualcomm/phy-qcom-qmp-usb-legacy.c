00632750ec01f978cd94d320d7638c2b7fb14aa8
Loic Poulain <loic.poulain@oss.qualcomm.com>
2026-01-21 15:28:25 +0100

    phy: qcom: qmp-usb-legacy: Fix possible NULL-deref on early runtime suspend
    There is a small window where the runtime suspend callback may run
    after pm_runtime_enable() and before pm_runtime_forbid(). In this
    case, a crash occurs because runtime suspend/resume dereferences
    qmp->phy pointer, which is not yet initialized:
            `if (!qmp->phy->init_count) {`
    
    This can also happen if user re-enables runtime-pm via the sysfs
    attribute before qmp phy is initialized.
    
    Similarly to other qcom phy drivers, introduce a qmp->phy_initialized
    variable that can be used to avoid relying on the possibly uninitialized
    phy pointer.
    
    Fixes: e464a3180a43 ("phy: qcom-qmp-usb: split off the legacy USB+dp_com support")
    Signed-off-by: Loic Poulain <loic.poulain@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260121142827.2583-5-loic.poulain@oss.qualcomm.com

--- drivers/phy/qualcomm/phy-qcom-qmp-usb-legacy.c
+++ drivers/phy/qualcomm/phy-qcom-qmp-usb-legacy.c
@@ -541,6 +541,7 @@ struct qmp_usb {
 	struct regulator_bulk_data *vregs;
 
 	enum phy_mode mode;
+	bool phy_initialized;
 
 	struct phy *phy;
 
@@ -895,6 +896,7 @@ static int qmp_usb_legacy_power_off(struct phy *phy)
 
 static int qmp_usb_legacy_enable(struct phy *phy)
 {
+	struct qmp_usb *qmp = phy_get_drvdata(phy);
 	int ret;
 
 	ret = qmp_usb_legacy_init(phy);
@@ -904,13 +906,18 @@ static int qmp_usb_legacy_enable(struct phy *phy)
 	ret = qmp_usb_legacy_power_on(phy);
 	if (ret)
 		qmp_usb_legacy_exit(phy);
+	else
+		qmp->phy_initialized = true;
 
 	return ret;
 }
 
 static int qmp_usb_legacy_disable(struct phy *phy)
 {
+	struct qmp_usb *qmp = phy_get_drvdata(phy);
 	int ret;
+
+	qmp->phy_initialized = false;
 
 	ret = qmp_usb_legacy_power_off(phy);
 	if (ret)
@@ -988,7 +995,7 @@ static int __maybe_unused qmp_usb_legacy_runtime_suspend(struct device *dev)
 
 	dev_vdbg(dev, "Suspending QMP phy, mode:%d\n", qmp->mode);
 
-	if (!qmp->phy->init_count) {
+	if (!qmp->phy_initialized) {
 		dev_vdbg(dev, "PHY not initialized, bailing out\n");
 		return 0;
 	}
@@ -1009,7 +1016,7 @@ static int __maybe_unused qmp_usb_legacy_runtime_resume(struct device *dev)
 
 	dev_vdbg(dev, "Resuming QMP phy, mode:%d\n", qmp->mode);
 
-	if (!qmp->phy->init_count) {
+	if (!qmp->phy_initialized) {
 		dev_vdbg(dev, "PHY not initialized, bailing out\n");
 		return 0;
 	}
