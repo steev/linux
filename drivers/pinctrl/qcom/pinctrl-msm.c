0814f82b43c46145d174ad9658bb07f1b875af6a
Stephan Gerhold <stephan.gerhold@linaro.org>
2026-05-26 16:24:42 +0530

    pinctrl: qcom: Acknowledge IRQs for PDC interrupt controller
    PDC needs to acknowledge incoming GPIO interrupts to clear the latched
    interrupt status in secondary mode of PDC. For level-triggered IRQs this
    happens automatically in irq_eoi() but for edge-triggered IRQs this needs
    to happen as early as possible in the IRQ handler.
    
    Implement this by using handle_fasteoi_ack_irq() as IRQ handler in this
    situation and forward the irq_ack() callback to the parent IRQ chip.
    
    Signed-off-by: Stephan Gerhold <stephan.gerhold@linaro.org>
    Signed-off-by: Maulik Shah <maulik.shah@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260526-hamoa_pdc-v2-6-f6857af1ce91@oss.qualcomm.com

--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -980,6 +980,16 @@ static void msm_gpio_irq_ack(struct irq_data *d)
 	if (test_bit(d->hwirq, pctrl->skip_wake_irqs)) {
 		if (test_bit(d->hwirq, pctrl->dual_edge_irqs))
 			msm_gpio_update_dual_edge_parent(d);
+
+		/*
+		 * During early initialization of the IRQ hierarchy,
+		 * irq_ack() is called by __irq_set_handler() before
+		 * the parent IRQ chip has been set up. This is why
+		 * we additionally need to check for d->parent_data->chip.
+		 */
+
+		if (d->parent_data->chip)
+			irq_chip_ack_parent(d);
 		return;
 	}
 
@@ -1054,7 +1064,10 @@ static int msm_gpio_irq_set_type(struct irq_data *d, unsigned int type)
 
 	if (test_bit(d->hwirq, pctrl->skip_wake_irqs)) {
 		clear_bit(d->hwirq, pctrl->dual_edge_irqs);
-		irq_set_handler_locked(d, handle_fasteoi_irq);
+		if (irqd_is_level_type(d))
+			irq_set_handler_locked(d, handle_fasteoi_irq);
+		else
+			irq_set_handler_locked(d, handle_fasteoi_ack_irq);
 		return 0;
 	}
 
